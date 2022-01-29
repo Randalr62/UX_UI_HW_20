1bc2503b652d13206ab3106ef8986332db83ffe9
Randalr62 <89607192+Randalr62@users.noreply.github.com>
2022-01-29 13:06:24 -0600

    8

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const mobileMenu= () => {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 }
- 
-    menu.addEventListener('click', mobileMenu);}
+
+menu.addEventListener('click', mobileMenu);}
